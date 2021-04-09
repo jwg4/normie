f66051a1e8eb295fecaf8dbdbed5015523366952
Jack Grahl <jack.grahl@gmail.com>
2021-04-09 01:33:23 +0100

    Add some new tests.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,5 +1,13 @@
-from normie import cdf
+from normie import cdf, invcdf
 
 
 def test_single_cdf_value():
     assert 0.97495 < cdf(0.96) < 0.97505
+
+
+def test_another_single_cdf_value():
+    assert 0.75485 < cdf(0.69) < 0.75495
+
+
+def test_single_invcdf_value():
+    assert 1.64485 < invcdf(0.95) < 1.64495

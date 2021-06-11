c0ae70117c9e44692b0ff1c7541e74c82c1be3e9
Jack Grahl <jack.grahl@gmail.com>
2021-06-12 00:10:50 +0100

    Add a test to complete.

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,3 +3,8 @@ from normie import cdf
 
 def test_single_cdf_value():
     assert 0.9745 < cdf(0.96) < 0.9755
+
+
+def test_single_value_from_excel():
+    assert 0.9745 < cdf(0.96) < 0.9755
+    assert not "Use correct values for this test."

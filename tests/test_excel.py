05cdf29743ee72f3b96c96726d12295cc5041e9c
Jack Grahl <jack.grahl@gmail.com>
2021-06-21 23:35:50 +0100

    Add another test.

--- tests/test_excel.py
+++ tests/test_excel.py
@@ -3,3 +3,7 @@ from normie.compat.excel import NORMINV
 
 def test_basic_value():
     assert 80.775 < NORMINV(.1, 100, 15) < 80.785
+
+
+def test_another_value():
+    assert 134.895 < NORMINV(.99, 100, 15) < 134.905

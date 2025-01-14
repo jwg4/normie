ae9a77cf8d8fc77e5505a65369369eba503d6b41
Jack Grahl <jack.grahl@gmail.com>
2025-01-14 17:07:40 +0000

    Define constants for these bounds.

--- tests/test_fuzz.py
+++ tests/test_fuzz.py
@@ -4,7 +4,14 @@ from hypothesis import given
 from hypothesis.strategies import floats
 
 
-@given(floats(-8.0, 8.0))
+# SMALLEST NUMBER SUCH THAT CDF IS POSITIVE FLOAT 
+CDF_MIN = -8.0
+
+# BIGGEST NUMBER WITH CDF DISTINGUISHABLE FROM 1
+CDF_MAX = 8.0
+
+
+@given(floats(CDF_MIN, CDF_MAX))
 def test_cdf_works(x):
     result = cdf(x)
     assert result is not None

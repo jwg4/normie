42311e00d96abcdc599c0a2edf766c6d11fa72f3
Jack Grahl <jack.grahl@gmail.com>
2025-01-18 12:09:41 +0000

    Many further bounds.

--- tests/test_bounds.py
+++ tests/test_bounds.py
@@ -1,6 +1,18 @@
 from normie import cdf, invcdf, pdf
 
 
-def test_cdf_at_infinity():
+def test_cdf_at_positive_infinity():
     assert cdf(float("inf")) == 1.0
+
+
+def test_cdf_at_negative_infinity():
+    assert cdf(float("-inf")) == 0.0
+
+
+def test_cdf_at_positive_infinity():
+    assert pdf(float("inf")) == 0.0
+
+
+def test_cdf_at_negative_infinity():
+    assert pdf(float("-inf")) == 0.0
 

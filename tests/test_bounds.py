d4a369db37d34458c7ccc66bab0f887b74488166
Jack Grahl <jack.grahl@gmail.com>
2025-01-18 12:10:49 +0000

    Add tests for invcdf.

--- tests/test_bounds.py
+++ tests/test_bounds.py
@@ -9,6 +9,14 @@ def test_cdf_at_negative_infinity():
     assert cdf(float("-inf")) == 0.0
 
 
+def test_invcdf_at_positive_infinity():
+    assert cdf(1.0) == float("inf")
+
+
+def test_invcdf_at_negative_infinity():
+    assert cdf(0.0) == float("-inf")
+
+
 def test_cdf_at_positive_infinity():
     assert pdf(float("inf")) == 0.0
 

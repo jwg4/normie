2b9c2be801bbc2fcda531071198318ab43b837e9
Jack Grahl <jack.grahl@gmail.com>
2025-01-21 15:25:55 +0000

    Typos cdf -> pdf.

--- normie/tests/test_bounds.py
+++ normie/tests/test_bounds.py
@@ -17,10 +17,10 @@ def test_invcdf_at_negative_infinity():
     assert invcdf(0.0) == float("-inf")
 
 
-def test_cdf_at_positive_infinity():
+def test_pdf_at_positive_infinity():
     assert pdf(float("inf")) == 0.0
 
 
-def test_cdf_at_negative_infinity():
+def test_pdf_at_negative_infinity():
     assert pdf(float("-inf")) == 0.0
 

02b2ee1eaa791fcd8345e031183c343f8c0de737
Jack Grahl <jack.grahl@gmail.com>
2025-03-03 22:41:08 +0000

    Separate header files.

--- normie_alternatives/src/normie.h
+++ normie_alternatives/src/normie.h
@@ -10,9 +10,3 @@ PyObject* pdf(PyObject*, PyObject*);
 PyObject* cdf(PyObject*, PyObject*);
 PyObject* invcdf(PyObject*, PyObject*);
 
-#ifndef NORMIE_PI
-#define NORMIE_PI 3.14159265358979323846
-#define RECI_SQRT_2_PI 0.3989422917366028
-#endif
-
-

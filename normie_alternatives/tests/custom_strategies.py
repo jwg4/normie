a9ee023692956435e73a95cb9fb51468a9e287f3
Jack Grahl <jack.grahl@gmail.com>
2025-10-12 00:51:31 +0100

    Test some properties of these functions.

---
+++ normie_alternatives/tests/custom_strategies.py
@@ -0,0 +1,7 @@
+from tests.constants import TEST_ERF_MAX, TEST_ERF_MIN
+
+from hypothesis.strategies import floats
+
+
+erf_floats = floats(min_value=TEST_ERF_MIN, max_value=TEST_ERF_MAX, allow_infinity=False, allow_nan=False)
+finite_floats = floats(allow_infinity=False, allow_nan=False)
---
+++ normie_alternatives/tests/test_validity.py
@@ -0,0 +1,23 @@
+from hypothesis import given
+import pytest
+
+from normie_alternatives.abram_steg.erf import erf_approx_7_1_25, erf_approx_7_1_26, erf_approx_7_1_27, erf_approx_7_1_28
+from tests.custom_strategies import erf_floats
+
+
+ERF_FUNCTIONS = [
+    erf_approx_7_1_25,
+    erf_approx_7_1_26,
+    erf_approx_7_1_27,
+    erf_approx_7_1_28
+]
+
+@given(x=erf_floats)
+@pytest.mark.parametrize(
+    "erf_function",
+    ERF_FUNCTIONS
+)
+def test_valid_erf(erf_function, x):
+    result = erf_function(x)
+    assert isinstance(result, float)
+    assert -1.0 <= result <= 1.0

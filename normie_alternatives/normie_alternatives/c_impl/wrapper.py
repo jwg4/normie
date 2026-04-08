3459dc8e79c54fc2ce9c16d3428e8ff8c1613b20
Jack Grahl <jack.grahl@gmail.com>
2026-04-09 00:53:57 +0100

    translate C function to decorated invcdf and inverf

--- normie_alternatives/normie_alternatives/c_impl/wrapper.py
+++ normie_alternatives/normie_alternatives/c_impl/wrapper.py
@@ -1,1 +1,7 @@
-from normie_impl import acklam_invcdf
+from normie_alternatives.function_types import inv_cdf_function
+from normie_alternatives.transformations import inv_cdf_to_inv_erf
+
+from normie_impl import acklam_invcdf as acklam_invcdf_impl
+
+acklam_invcdf = inv_cdf_function(acklam_invcdf_impl)
+acklam_inverf = inv_cdf_to_inv_erf(acklam_invcdf)

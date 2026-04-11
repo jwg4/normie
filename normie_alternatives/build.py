1ffb2d84b3870278bb67fe2e0f2fbbb3b0ceb95f
Jack Grahl <jack.grahl@gmail.com>
2026-04-12 00:13:53 +0100

    Add manual build script to compile C sources.

---
+++ normie_alternatives/build.py
@@ -0,0 +1,30 @@
+"""Build script."""
+from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
+
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+
+extensions = [
+    Extension("normie_impl", sources=["src/normie_impl.c", "src/acklam.c"]),
+]
+
+class BuildFailed(Exception):
+    pass
+
+
+class ExtBuilder(build_ext):
+    def run(self):
+        try:
+            build_ext.run(self)
+        except (DistutilsPlatformError, FileNotFoundError):
+            pass
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except (CCompilerError, DistutilsExecError, DistutilsPlatformError, ValueError):
+            pass
+
+
+def build(setup_kwargs):
+    setup_kwargs.update({"ext_modules": extensions, "cmdclass": {"build_ext": ExtBuilder}})

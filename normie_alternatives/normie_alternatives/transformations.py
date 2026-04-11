9f9670a753b3b19ee3393191021064329f2274cf
Jack Grahl <jack.grahl@gmail.com>
2026-04-12 00:13:05 +0100

    Correct type of function.

--- normie_alternatives/normie_alternatives/transformations.py
+++ normie_alternatives/normie_alternatives/transformations.py
@@ -2,7 +2,7 @@ from normie_alternatives.function_types import FunctionTypes, inv_erf_function
 
 
 def inv_cdf_to_inv_erf(inv_cdf_fn):
-    assert hasattr(inv_cdf_fn, "function_type") and inv_cdf_fn.function_type == FunctionTypes.CDF_FUNCTION, "inv_cdf_fn must be a CDF function"
+    assert hasattr(inv_cdf_fn, "function_type") and inv_cdf_fn.function_type == FunctionTypes.INV_CDF_FUNCTION, "inv_cdf_fn must be a CDF function"
 
     def inv_erf_fn(x):
         return inv_cdf_fn((x + 1) / 2.0)

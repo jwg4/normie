d4752177f1b436366148427528bbf503cef0fbd5
Jack Grahl <jack.grahl@gmail.com>
2026-04-12 00:18:15 +0100

    Make the builtin function into a python function.

--- normie_alternatives/normie_alternatives/c_impl/wrapper.py
+++ normie_alternatives/normie_alternatives/c_impl/wrapper.py
@@ -3,5 +3,5 @@ from normie_alternatives.transformations import inv_cdf_to_inv_erf
 
 from normie_impl import acklam_invcdf as acklam_invcdf_impl
 
-acklam_invcdf = inv_cdf_function(acklam_invcdf_impl)
+acklam_invcdf = inv_cdf_function(lambda *args, **kwargs: acklam_invcdf_impl(*args, **kwargs))
 acklam_inverf = inv_cdf_to_inv_erf(acklam_invcdf)
